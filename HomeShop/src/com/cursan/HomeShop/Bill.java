384e8a88176046463c99531f38e42a0c4ba880b9
ludovic <ludovic.ch.lc@gmail.com>
2020-03-19 15:00:12 +0100

    cours Java avec le projet HomeShop - après leçon héritage

--- HomeShop/src/com/cursan/HomeShop/Bill.java
+++ HomeShop/src/com/cursan/HomeShop/Bill.java
@@ -1,4 +1,28 @@
 package com.cursan.HomeShop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bill {
+
+    private Customer customers;
+    private Map<String, Integer> products = new HashMap<>();
+
+    public Bill(Customer customers, Map<String, Integer> products) {
+        this.customers = customers;
+        this.products = products;
+    }
+
+    public Customer getCustomers() {
+        return customers;
+    }
+
+    public Map<String, Integer> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product produit, Integer quantity) {
+
+
+    }
 }
