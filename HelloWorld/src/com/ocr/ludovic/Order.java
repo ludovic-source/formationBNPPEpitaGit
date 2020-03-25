94988e8131bbb19bdbe78e37f06789806d1040e2
ludovic <ludovic.ch.lc@gmail.com>
2020-03-25 14:10:15 +0100

    cours OpenClassRooms - démarrez votre projet avec Java - leçon répétez une action

--- HelloWorld/src/com/ocr/ludovic/Order.java
+++ HelloWorld/src/com/ocr/ludovic/Order.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Order {
 
-    private final String[] menu = {"poulet", "boeuf", "végétarien"};
+    public final String[] menu = {"poulet", "boeuf", "végétarien"};
     private int menuChoisi;
+    public final String[] side = {"légumes frais", "frites", "riz", "tout"};
+    public final String[] boisson = {"eau plate", "eau gazeuse", "soda"};
 
     /**
      * Display all available menus in the restaurant.
@@ -30,14 +32,56 @@ public class Order {
     }
 
     public void runMenu() {
-        /* Order commande = new Order(); */ // inutile si on utilise le pré fix this. à la place du nom de l'objet
+        /* Order commande = new Order(); */ // inutile si on utilise le pré fixe this. à la place du nom de l'objet
 
         this.displayAvailableMenus();
 
+        // choix du menu
         Scanner sc = new Scanner(System.in);
         int menuChoisi = sc.nextInt();
-
         this.displaySelectedMenu(menuChoisi);
+
+        // choix de l'accompagnement
+        this.displayAvailableSide();
+        this.displaySelectedSide(sc.nextInt());
+
+        // choix de la boisson
+        if (menuChoisi != 2) {
+            this.displayAvailableBoisson();
+            this.displaySelectedBoisson(sc.nextInt());
+        }
+
+    }
+
+    public void displaySelectedBoisson(int choixBoisson) {
+
+        System.out.println("Vous avez choisi comme accompagnement : " + boisson[choixBoisson-1]);
+
+    }
+
+    public void displayAvailableBoisson() {
+        System.out.println("");
+        System.out.println("Choix boisson");
+        for (int i = 0; i < boisson.length; i++) {
+            System.out.println(i+1 + " - " + boisson[i]);
+        }
+        System.out.println("Que souhaitez-vous comme boisson ?");
+
+    }
+
+    public void displayAvailableSide() {
+        System.out.println("");
+        System.out.println("Choix accompagnement");
+        for (int i = 0; i < side.length; i++) {
+            System.out.println(i+1 + " - " + side[i]);
+        }
+        System.out.println("Que souhaitez-vous comme accompagnement ?");
+
+    }
+
+    public void displaySelectedSide(int numberSide) {
+
+        System.out.println("Vous avez choisi comme accompagnement : " + side[numberSide-1]);
 
     }
 
