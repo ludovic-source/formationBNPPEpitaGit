50f20874ed69c7d7a07b736c5786b344682b4c12
ludovic <ludovic.ch.lc@gmail.com>
2020-03-25 10:21:35 +0100

    cours OpenClassRooms - démarrez votre projet avec Java - leçon importez votre 1er projet

---
+++ HelloWorld/src/com/ocr/ludovic/Main.java
@@ -0,0 +1,14 @@
+package com.ocr.ludovic;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Bienvenue au resto OpenClassRooms !");
+
+	    Order commande = new Order();
+
+	    commande.displayAvailableMenus();
+	    commande.displaySelectedMenu(3);
+
+    }
+}
---
+++ HelloWorld/src/com/ocr/ludovic/Order.java
@@ -0,0 +1,26 @@
+package com.ocr.ludovic;
+
+public class Order {
+
+    /**
+     * Display all available menus in the restaurant.
+     */
+    public void displayAvailableMenus() {
+
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet ");
+        System.out.println("2 - boeuf ");
+        System.out.println("3 - végétarien ");
+        System.out.println("Que souhaitez-vous comme menu ?");
+
+    }
+
+    /**
+     * Display a selected menu.
+     * @param nbMenu The selected menu.
+     */
+    public void displaySelectedMenu(int nbMenu) {
+        System.out.println("Vous avez choisi le menu " + nbMenu);
+    }
+
+}
