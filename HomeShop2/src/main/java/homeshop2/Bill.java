f39651a534336de2fea0ddbaad870f9e8c16028f
ludovic <ludovic.ch.lc@gmail.com>
2020-03-27 16:01:22 +0100

    cours OpenClassRooms Java et l'objet - nouvelle version homeshop2 qui fonctionne avec Jetty - générer une facture à partir du formulaire

--- HomeShop2/src/main/java/homeshop2/Bill.java
+++ HomeShop2/src/main/java/homeshop2/Bill.java
@@ -42,7 +42,7 @@ public class Bill {
         String ligneEspace = "-----------------------------------------------------";
 
         ArrayList<String> lignesAEcrire = new ArrayList<>();
-        Path path = Paths.get("C:/Users/stagiaire/IdeaProjects/HomeShop2/src/com/cursan/HomeShop2/facture.txt");
+        Path path = Paths.get("C:/Users/stagiaire/IdeaProjects/HomeShop2/src/com/cursan/facture.txt");
 
         try {
             boolean fichierExists = Files.exists(path);
@@ -96,6 +96,47 @@ public class Bill {
             total += product.getPrice() * quantity;
         }
         return total;
+    }
+
+    public ArrayList<String> generateBillDematerialisee() {
+
+        // formatage des lignes fixes de la facture
+        String ligneEntete1 = "HomeShop compagnie";
+        String ligneEntete2 = "1 Place Charles de Gaulle, 75008 Paris";
+        String ligneEntete3 = "Facture à l'attention de :";
+        String ligneTitreProduits = "Produits: ";
+        String ligneEspace = "-----------------------------------------------------";
+
+        ArrayList<String> lignesAEcrire = new ArrayList<>();
+
+        lignesAEcrire.add(ligneEntete1);
+        lignesAEcrire.add(ligneEntete2);
+        lignesAEcrire.add("");
+        lignesAEcrire.add(ligneEntete3);
+        lignesAEcrire.add(this.customers.getFullname());
+        lignesAEcrire.add(this.customers.getAdress());
+        lignesAEcrire.add("");
+        lignesAEcrire.add("Mode de livraison : " + this.delivery.getInfo() + " " + this.delivery.getPrice() + " €");
+        lignesAEcrire.add("");
+        lignesAEcrire.add(ligneTitreProduits);
+        lignesAEcrire.add(ligneEspace);
+
+        Map<Product, Integer> products = getProducts();
+
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+            lignesAEcrire.add(product.getName() + " - " + product.getPrice() + " - " + quantity + " unité(s)");
+            lignesAEcrire.add(product.getDescription());
+            lignesAEcrire.add(ligneEspace);
+        }
+
+        lignesAEcrire.add("Livraison : " + this.delivery.getPrice());
+        lignesAEcrire.add(ligneEspace);
+        lignesAEcrire.add("Total : " + getTotal());
+
+        return lignesAEcrire;
+
     }
 
 }
--- HomeShop2/src/main/java/homeshop2/web/BillServlet.java
+++ HomeShop2/src/main/java/homeshop2/web/BillServlet.java
@@ -1,8 +1,6 @@
 package homeshop2.web;
 
-import homeshop2.Fridge;
-import homeshop2.Product;
-import homeshop2.Television;
+import homeshop2.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BillServlet extends HttpServlet {
 
@@ -33,14 +33,13 @@ public class BillServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");   // pour indiquer qu'on va utiliser du HTML pour notre réponse
-        if (req.getQueryString() == null)   // req.getQueryString() permet de récupérer les paramètres contenus dans l'URL d'appel GET
-            displayForm(resp);              // pas de paramètres, on affiche le formulaire à remplir
-        else
+        resp.setContentType("text/html");       // pour indiquer qu'on va utiliser du HTML pour notre réponse
+        if (req.getQueryString() == null) {     // req.getQueryString() permet de récupérer les paramètres contenus dans l'URL d'appel GET
+            displayForm(resp);                  // pas de paramètres, on affiche le formulaire à remplir
+        }
+        else {
             displayBill(req, resp);         // pour afficher la facture correspondant au formulaire rempli
-    }
-
-    private void displayBill(HttpServletRequest req, HttpServletResponse resp) {
+        }
     }
 
     private void displayForm(HttpServletResponse resp) throws IOException {
@@ -64,6 +63,89 @@ public class BillServlet extends HttpServlet {
                 "<input type=\"submit\"/>" +
                 "</form>";
         resp.getWriter().println(form);
+    }
+
+    private void displayBill(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        Map<String, String> params = splitParameters(req.getQueryString());
+
+        Customer customer = new Customer(params.get("fullname"), params.get("address"));
+
+        Delivery delivery = null;
+
+        switch (params.get("deliveryMode")) {
+            case "direct" :
+                delivery = new DirectDelivery();
+                break;
+            case "express" :
+                delivery = new ExpressDelivery(params.get("deliveryInfo"));
+                break;
+            case "relay" :
+                delivery = new RelayDelivery(Integer.parseInt(params.get("deliveryInfo")));
+                break;
+            case "takeAway" :
+                delivery = new TakeAwayDelivery();
+                break;
+        }
+
+        Bill bill = new Bill(customer, delivery);
+
+        String produitsCommandes = params.get("products");
+        String[] tableauProduitsCommandes = produitsCommandes.split("%0D%0A");
+        Map<Integer, Integer> mapProduitsCommandes = new HashMap<>();
+        int numeroProduit;
+        int quantity;
+        for (String ligneCommande : tableauProduitsCommandes) {
+            String[] tableauProduitQuantite = ligneCommande.split("%3A");
+            numeroProduit = Integer.parseInt(tableauProduitQuantite[0]);
+            quantity = Integer.parseInt(tableauProduitQuantite[1]);
+            bill.addProduct(products.get(numeroProduit), quantity);
+        }
+
+        // générer la facture
+        // bill.generateBill();
+        ArrayList<String> lignesAEcrire = bill.generateBillDematerialisee();
+        StringBuilder facture = new StringBuilder();
+        for (String chaqueLigne : lignesAEcrire) {
+            facture.append("<br/>").append(chaqueLigne);
+        }
+        resp.getWriter().println(facture);
+
+    }
+
+    public Map<String, String> splitParameters(String queryString) {
+
+        int compteurParametre = 0;
+
+        String[] tableauParametre = queryString.split("[?&=]");
+        Map<String, String> mapParametre = new HashMap<>();
+
+        for (String parametre : tableauParametre) {
+
+            if (parametre.equals("fullname") || parametre.equals("address") || parametre.equals("deliveryMode") || parametre.equals("deliveryInfo") || parametre.equals("products")) {
+                mapParametre.put(parametre, tableauParametre[compteurParametre+1]);
+            }
+            compteurParametre += 1;
+
+        }
+
+
+        return mapParametre;
+
+       // http://localhost:8080/bill?fullname=Juste+Leblanc&address=19+rue+Germain+Pilon&deliveryMode=express
+       //         &deliveryInfo=Paris&products=1%3A2%0D%0A2%3A1
+        // 0 http://localhost:8080/bill
+        // 1 fullname
+        // 2 Juste+Leblanc
+        // 3 address
+        // 4 19+rue+Germain+Pilon
+        // 5 deliveryMode
+        // 6 express
+       //  7 deliveryInfo=
+        // 8 Paris
+        // 9 products
+        // 10 1%3A2%0D%0A2%3A1   %0D%0A = retour à ligne et %3A = :   ==> 1:2 + 2:1
+
     }
 
 }
