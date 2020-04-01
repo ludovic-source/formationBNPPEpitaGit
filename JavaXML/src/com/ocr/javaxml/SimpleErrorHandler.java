e22dcef6b919e2915ed895b63f2f928af7467d12
ludovic <ludovic.ch.lc@gmail.com>
2020-04-01 10:14:07 +0200

    cours OpenClassRooms - java et le XML - fin chapitre à la découverte de DOM

---
+++ JavaXML/src/com/ocr/javaxml/SimpleErrorHandler.java
@@ -0,0 +1,24 @@
+package com.ocr.javaxml;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class SimpleErrorHandler implements ErrorHandler {
+    @Override
+    public void warning(SAXParseException e) throws SAXException {
+        System.out.println("WARNING : " + e.getMessage());
+    }
+
+    @Override
+    public void error(SAXParseException e) throws SAXException {
+        System.out.println("ERROR : " + e.getMessage());
+        throw e;
+    }
+
+    @Override
+    public void fatalError(SAXParseException e) throws SAXException {
+        System.out.println("FATAL ERROR : " + e.getMessage());
+        throw e;
+    }
+}
